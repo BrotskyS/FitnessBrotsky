108caf133b5dcc68df04e5a91108e34ff41da669
BrotskyS <sergiypik.work@gmail.com>
2022-03-30 21:30:16 +0300

    Initial Commit

---
+++ FitnessBrotsky/FitnessBrotskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessBrotskyApp.swift
+//  FitnessBrotsky
+//
+//  Created by Sergiy Brotsky on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FitnessBrotskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
